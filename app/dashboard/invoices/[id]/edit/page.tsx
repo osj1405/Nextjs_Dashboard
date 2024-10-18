3b79f186eec10b3c60748a214fe1c5769194472a
osj1405 <osj1405@naver.com>
2024-10-18 15:04:43 +0900

    form validation:

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -2,6 +2,7 @@ import Form from "@/app/ui/invoices/edit-form";
 import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
 import { fetchCustomers, fetchInvoiceById } from "@/app/lib/data";
 import { invoices } from "@/app/lib/placeholder-data";
+import { notFound } from "next/navigation";
 
 export default async function Page({params}:{
     params: {id: string}
@@ -11,6 +12,10 @@ export default async function Page({params}:{
         fetchInvoiceById(id),
         fetchCustomers(),
     ]);
+
+    if(!invoice){
+        notFound();
+    }
     return (
         <main>
             <Breadcrumbs 
